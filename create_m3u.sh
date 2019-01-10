abd17d599bddf81ee9a6cd4d3eca75d55b7824a5
Steven Weijie Ding <wjdingdev@gmail.com>
2019-01-10 23:08:07 +0800

    Initial commit

---
+++ create_m3u.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cat data/channel.csv | sed -e "s/,/\x09/; s/^/#EXTINF:0,/; s/\x09/\\n/" > koditv-udp.m3u
+cat data/channel.csv | sed -e "s/,/\x09/; s/^/#EXTINF:0,/; s/\x09/\\n/; s/rtp:\//http:\/\/filesvr.lan:4022\/rtp/" > koditv-tcp.m3u
+
---
+++ create_vlc_xspf.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+cat data/channel.csv | awk -F ',' '
+BEGIN {
+	OFS=""
+	i=0
+	print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+	print "<playlist xmlns=\"http://xspf.org/ns/0/\" xmlns:vlc=\"http://www.videolan.org/vlc/playlist/ns/0/\" version=\"1\">"
+	print "\t<title>Playlist</title>"
+	print "\t<trackList>"
+} 
+
+{
+	gsub("rtp:/", "http://192.168.2.1:4022/rtp", $2)
+	print "\t\t<track>"
+	print "\t\t\t<title>", $1, "</title>"
+	print "\t\t\t<location>", $2, "</location>"
+	print "\t\t\t<extension application=\"http://www.videolan.org/vlc/playlist/0\">"
+	print "\t\t\t\t<vlc:id>", i, "</vlc:id>"
+	print "\t\t\t</extension>"
+	print "\t\t</track>";
+	i=i+1;
+}
+
+END {
+	print "\t</trackList>\n";
+	print "\t<extension application=\"http://www.videolan.org/vlc/playlist/0\">";
+	for (j=0; j<=i; j++) {
+		print "\t\t<vlc:item tid=\"", j, "\"/>";
+	}
+	print "\t</extension>";
+	print "</playlist>";
+}
+' > vlc-tcp.xspf
+
